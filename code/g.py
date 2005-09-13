5bb1930d57f4a22d6eaf663f44276ba6b05af453
phil <phil@b907cd06-5c16-4c62-a1d0-e7f4b8dee1c7>
2005-09-13 03:06:08 +0000

    * /trunk/code/g.py:    Use the event.unicode value for keys instead of the wacky lookup-fu I    was using.  Thanks, emh!

--- code/g.py
+++ code/g.py
@@ -186,45 +186,11 @@ def create_dialog(string_to_print, box_font, xy, size, bg_color, out_color, text
 			elif event.type == pygame.MOUSEMOTION:
 				sel_button = buttons.refresh_buttons(sel_button, menu_buttons, event)
 
-valid_input_characters = {
-	pygame.K_a: 'a',
-	pygame.K_b: 'b',
-	pygame.K_c: 'c',
-	pygame.K_d: 'd',
-	pygame.K_e: 'e',
-	pygame.K_f: 'f',
-	pygame.K_g: 'g',
-	pygame.K_h: 'h',
-	pygame.K_i: 'i',
-	pygame.K_j: 'j',
-	pygame.K_k: 'k',
-	pygame.K_l: 'l',
-	pygame.K_m: 'm',
-	pygame.K_n: 'n',
-	pygame.K_o: 'o',
-	pygame.K_p: 'p',
-	pygame.K_q: 'q',
-	pygame.K_r: 'r',
-	pygame.K_s: 's',
-	pygame.K_t: 't',
-	pygame.K_u: 'u',
-	pygame.K_v: 'v',
-	pygame.K_w: 'w',
-	pygame.K_x: 'x',
-	pygame.K_y: 'y',
-	pygame.K_z: 'z',
-	pygame.K_0: '0',
-	pygame.K_1: '1',
-	pygame.K_2: '2',
-	pygame.K_3: '3',
-	pygame.K_4: '4',
-	pygame.K_5: '5',
-	pygame.K_6: '6',
-	pygame.K_7: '7',
-	pygame.K_8: '8',
-	pygame.K_9: '9',
-	pygame.K_SPACE: ' '
-}
+valid_input_characters = ('a','b','c','d','e','f','g','h','i','j','k','l','m',
+			  'n','o','p','q','r','s','t','u','v','w','x','y','z',
+			  'A','B','C','D','E','F','G','H','I','J','K','L','M',
+			  'N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
+			  '0','1','2','3','4','5','6','7','8','9','.',' ')
 
 def create_textbox(starting_text, box_font, xy, size, max_length, bg_color, out_color, text_color):
 	screen.fill(out_color, (xy[0], xy[1], size[0], size[1]))
@@ -252,12 +218,9 @@ def create_textbox(starting_text, box_font, xy, size, max_length, bg_color, out_
 						work_string = work_string[:-1]
 						cursor_loc -= 1
 						need_redraw = 1
-				elif event.key in valid_input_characters:
+				elif event.unicode in valid_input_characters:
 					if cursor_loc < max_length:
-						char_to_add = valid_input_characters[event.key]
-						if event.mod & pygame.KMOD_SHIFT != 0:
-							char_to_add = char_to_add.upper()
-						work_string += char_to_add
+						work_string += event.unicode
 						cursor_loc += 1
 						need_redraw = 1
 			if need_redraw:
