fbfcdbefeccb1a0b40776d494ddeba590e346c09
phil <phil@b907cd06-5c16-4c62-a1d0-e7f4b8dee1c7>
2006-03-05 18:01:13 +0000

    * /trunk/code/base_screen.py:    Moved around a lot of the stuff on the map screen.  This is partly for    usability (although I'm still not happy with the two lines at the    bottom), and partly because I'm about to add a display at the top of    the base name.

--- code/base_screen.py
+++ code/base_screen.py
@@ -36,15 +36,16 @@ def show_base(base):
     menu_buttons.append(buttons.make_norm_button((0, 0), (70, 25),
         "BACK", 0, g.font[1][20]))
 
-    menu_buttons.append(buttons.button((70, -1), (g.screen_size[0]-70, 26),
-        "DETECTION CHANCE", -1, g.colors["black"], g.colors["dark_blue"],
-        g.colors["black"], g.colors["white"], g.font[1][15]))
+    menu_buttons.append(buttons.button((0, g.screen_size[1]-25),
+        (g.screen_size[0], 25), "DETECTION CHANCE", -1,
+        g.colors["black"], g.colors["black"], g.colors["black"],
+        g.colors["white"], g.font[1][15]))
 
-    menu_buttons.append(buttons.make_norm_button((0, g.screen_size[1]-50),
+    menu_buttons.append(buttons.make_norm_button((0, g.screen_size[1]-75),
         (170, 25), "CHANGE RESEARCH", 0, g.font[1][20], "CHANGE"))
 
-    menu_buttons.append(buttons.button((0, g.screen_size[1]-25),
-        (g.screen_size[0]-40, 26),
+    menu_buttons.append(buttons.button((0, g.screen_size[1]-50),
+        (g.screen_size[0], 25),
         "STUDYING:", -1, g.colors["black"], g.colors["dark_blue"],
         g.colors["black"], g.colors["white"], g.font[1][15]))
 
@@ -65,13 +66,13 @@ def show_base(base):
     menu_buttons[7].activate_key = "s"
 
     menu_buttons.append(buttons.make_norm_button((g.screen_size[0]-40,
-        g.screen_size[1]-25), (20, 25), "<", 0, g.font[1][20]))
+        0), (20, 25), "<", 0, g.font[1][20]))
 
     menu_buttons.append(buttons.make_norm_button((g.screen_size[0]-20,
-        g.screen_size[1]-25), (20, 25), ">", 0, g.font[1][20]))
+        0), (20, 25), ">", 0, g.font[1][20]))
 
     menu_buttons.append(buttons.make_norm_button((g.screen_size[0]-90,
-        g.screen_size[1]-50), (90, 25), "DESTROY", 0, g.font[1][20]))
+        g.screen_size[1]-75), (90, 25), "DESTROY", 0, g.font[1][20]))
 
     sel_button = -1
 
