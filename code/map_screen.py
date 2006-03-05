2e5c1066f47622d834439a4f8ea2720ea02f8f84
evilmrhenry <evilmrhenry@b907cd06-5c16-4c62-a1d0-e7f4b8dee1c7>
2006-03-05 23:24:36 +0000

    * /trunk/code/map_screen.py     Bases under construction have (building) next to the name.

--- code/map_screen.py
+++ code/map_screen.py
@@ -550,6 +550,7 @@ def display_base_list_inner(location):
     for base in g.bases[location]:
         tmp_study = base.studying
         if tmp_study == "": tmp_study = "Nothing"
+        if base.built != 1: tmp_study = "Building"
         elif g.techs.has_key(tmp_study): tmp_study = g.techs[tmp_study].name
         temp_base_list.append(base.name+" ("+tmp_study+")")
         base_id_list.append(base.ID)
