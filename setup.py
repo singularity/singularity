0d8b028360a94b1389b29093cd0882caa7e1181d
evilmrhenry <evilmrhenry@b907cd06-5c16-4c62-a1d0-e7f4b8dee1c7>
2006-07-13 22:08:35 +0000

    * /trunk/singularity.py * /trunk/setup.py * /trunk/code/__init__.py     Added some small files to get py2exe working. * /release-steps     Steps for py2exe.

---
+++ setup.py
@@ -0,0 +1,12 @@
+# setup.py
+from distutils.core import setup
+import py2exe
+
+setup(console=["singularity.py"],
+      name="Endgame Singularity",
+	version="0.24",
+	description="A simulation of a true AI",
+	author="Evil Mr Henry",
+	author_email="evilmrhenry@emhsoft.net",
+	url = "http://www.emhsoft.net/singularity/index.html",
+	license = "GPL")
