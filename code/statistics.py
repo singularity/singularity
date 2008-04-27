559809fd48b9f69eac4cbad636a526a5c02899f9
funnyman3595 <funnyman3595@b907cd06-5c16-4c62-a1d0-e7f4b8dee1c7>
2008-04-27 06:41:17 +0000

    Adding header to code/statistics.py

--- code/statistics.py
+++ code/statistics.py
@@ -1,3 +1,23 @@
+#file: statistics.py
+#Copyright (C) 2008 FunnyMan3595
+#This file is part of Endgame: Singularity.
+
+#Endgame: Singularity is free software; you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation; either version 2 of the License, or
+#(at your option) any later version.
+
+#Endgame: Singularity is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+
+#You should have received a copy of the GNU General Public License
+#along with Endgame: Singularity; if not, write to the Free Software
+#Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+#This file contains the Statistics class, used for saving/loading single-game
+#statistics.
 class Statistics(object):
     def get_statistic(self, name):
         return 0
