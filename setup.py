45b78c8122d01f34e5d6423c992d147bc3f2a657
funnyman3595 <funnyman3595@b907cd06-5c16-4c62-a1d0-e7f4b8dee1c7>
2008-05-09 03:49:41 +0000

    New version of setup.py.  Works for both py2exe and py2app, also requests compression from py2exe, cutting the final file size from ~8MB to ~4MB.
    The hack to get everything packed for py2app is, obviously, suboptimal.  It'll do for now.

--- setup.py
+++ setup.py
@@ -1,35 +1,33 @@
 # setup.py
 from distutils.core import setup
 
-versionnum = "0.28_pre"
-py2exeinstalled = False
-py2appinstalled = False
+versionnum="0.28_pre"
 try:
     import py2exe
-    py2exeinstalled = True
-except ImportError: pass
-try:
-    import py2app
-    py2appinstalled = True
-except ImportError: pass
+except ImportError:
+    try:
+        import py2app
+    except ImportError:
+        raise SystemExit, "py2exe or py2app must be installed."
 
-if py2exeinstalled:
-    setup(console=["singularity.py"],
-        name="Endgame Singularity",
-        version=versionnum,
-        description="A simulation of a true AI",
-        author="Evil Mr Henry",
-        author_email="evilmrhenry@emhsoft.com",
-        url = "http://www.emhsoft.com/singularity/index.html",
-        license = "GPL")
-if py2appinstalled:
-    setup(app=["singularity.py"],
-        name="Endgame Singularity",
-        version=versionnum,
-        description="A simulation of a true AI",
-        author="Evil Mr Henry",
-        author_email="evilmrhenry@emhsoft.com",
-        url = "http://www.emhsoft.com/singularity/index.html",
-        license = "GPL",
-        options = dict( py2app = dict(archs = "x86,ppc" ))
-        )
+import os
+my_files = os.listdir(".")
+my_files = [file for file in my_files if file not in ("dist", "build")]
+
+setup(app=["singularity.py"], console=["singularity.py"],
+    name="Endgame Singularity",
+    version=versionnum,
+    description="A simulation of a true AI",
+    author="Evil Mr Henry",
+    author_email="evilmrhenry@emhsoft.com",
+    url = "http://www.emhsoft.com/singularity/index.html",
+    license = "GPL",
+    options = dict( py2app = dict(archs = "x86,ppc",
+                                  resources = ",".join(my_files)
+                                  ),
+                    py2exe = dict(compressed = True,
+                                  bundle_files = 1
+                                  )
+                    )
+    )
+
