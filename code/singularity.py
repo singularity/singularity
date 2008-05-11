f5867d47ca780d06ba17efecb194998eb3f3922e
evilmrhenry <evilmrhenry@b907cd06-5c16-4c62-a1d0-e7f4b8dee1c7>
2008-05-11 20:45:03 +0000

    * /trunk/code/singularity.py 	Updated version #

--- code/singularity.py
+++ code/singularity.py
@@ -33,7 +33,7 @@ A game by Evil Mr Henry and Phil Bordelon; released under the GPL. Copyright 200
 Website: http://www.emhsoft.com/singularity/
 IRC Room: #singularity on irc.oftc.net (port 6667)
 
-Version 0.28_pre"""
+Version 0.28"""
 
 pygame.mixer.pre_init(48000, -16, 2)
 pygame.init()
