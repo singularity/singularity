b73474a34529db9ea778a66b0b48e541ed36ac2b
funnyman3595 <funnyman3595@b907cd06-5c16-4c62-a1d0-e7f4b8dee1c7>
2008-08-02 22:32:44 +0000

    GUI rewrite, trimming the two copies of g.py to their relevant pieces.

--- code/g.py
+++ code/g.py
@@ -19,7 +19,6 @@
 
 #This file contains all global objects.
 
-import clock as sing_clock
 import ConfigParser
 import pygame
 import os
@@ -37,9 +36,6 @@ from buttons import always, void, exit
 
 stats = statistics.Statistics()
 
-#screen is the actual pygame display.
-global screen
-
 # Useful constants.
 hours_per_day = 24
 minutes_per_hour = 60
@@ -47,11 +43,6 @@ minutes_per_day = 24 * 60
 seconds_per_minute = 60
 seconds_per_hour = 60 * 60
 seconds_per_day = 24 * 60 * 60
-
-#size of the screen. This can be set via command-line option.
-screen_size = (800, 600)
-
-clock = sing_clock.Clock()
 
 #Allows access to the cheat menu.
 cheater = 0
@@ -100,22 +91,6 @@ data_loc = "../data/"
 
 def quit_game():
     sys.exit()
-
-#colors:
-colors = {}
-
-def fill_colors():
-    colors["white"] = (255, 255, 255, 255)
-    colors["black"] = (0, 0, 0, 255)
-    colors["red"] = (255, 0, 0, 255)
-    colors["green"] = (0, 255, 0, 255)
-    colors["blue"] = (0, 0, 255, 255)
-    colors["dark_red"] = (125, 0, 0, 255)
-    colors["dark_green"] = (0, 125, 0, 255)
-    colors["dark_blue"] = (0, 0, 125, 255)
-    colors["light_red"] = (255, 50, 50, 255)
-    colors["light_green"] = (50, 255, 50, 255)
-    colors["light_blue"] = (50, 50, 255, 255)
 
 strings = {}
 help_strings = {}
@@ -287,266 +262,6 @@ def play_music(musicdir="music"):
         delay_time = 0
         pygame.mixer.music.load(random.choice(music_dict[musicdir]))
         pygame.mixer.music.play()
-
-#
-# Font functions.
-#
-
-#Normal and Acknowledge fonts.
-font = []
-font.append([0] * 51)
-font.append([0] * 51)
-
-#given a surface, string, font, char to underline (int; -1 to len(string)),
-#xy coord, and color, print the string to the surface.
-#Align (0=left, 1=Center, 2=Right) changes the alignment of the text
-def print_string(surface, string_to_print, font, underline_char, xy, color, align=0):
-    if align != 0:
-        size = font.size(string_to_print)
-        if align == 1: xy = (xy[0] - size[0]/2, xy[1])
-        elif align == 2: xy = (xy[0] - size[0], xy[1])
-    if underline_char == -1 or underline_char >= len(string_to_print):
-        text = font.render(string_to_print, 1, color)
-        surface.blit(text, xy)
-    else:
-        text = font.render(string_to_print[:underline_char], 1, color)
-        surface.blit(text, xy)
-        size = font.size(string_to_print[:underline_char])
-        xy = (xy[0] + size[0], xy[1])
-        font.set_underline(1)
-        text = font.render(string_to_print[underline_char], 1, color)
-        surface.blit(text, xy)
-        font.set_underline(0)
-        size = font.size(string_to_print[underline_char])
-        xy = (xy[0] + size[0], xy[1])
-        text = font.render(string_to_print[underline_char+1:], 1, color)
-        surface.blit(text, xy)
-
-#Used to display descriptions and such. Automatically wraps the text to fit
-#within a certain width.
-def print_multiline(surface, string_to_print, font, width, xy, color):
-    start_xy = xy
-    string_array = string_to_print.replace("\n", " \\n ").split(" ")
-
-    for string in string_array:
-        string += " "
-        size = font.size(string)
-
-        if string == "\\n ":
-            xy = (start_xy[0], xy[1]+size[1])
-            continue
-        text = font.render(string, 1, color)
-
-        if (xy[0]-start_xy[0])+size[0] > width:
-            xy = (start_xy[0], xy[1]+size[1])
-        surface.blit(text, xy)
-        xy = (xy[0]+size[0], xy[1])
-
-#create dialog with OK button.
-def create_dialog(string_to_print, box_font = None, xy = None, size = (250,250),
-                  bg_color = None, out_color = None, text_color = None):
-    # Defaults that reference other variables, which may not be initialized when
-    # the function is defined.
-    if box_font == None:
-        box_font = font[0][18]
-    if xy == None:
-        xy = ( (screen_size[0] / 2) - 100, 50)
-    if bg_color == None:
-        bg_color = colors["dark_blue"]
-    if out_color == None:
-        out_color = colors["white"]
-    if text_color == None:
-        text_color = colors["white"]
-
-    screen.fill(out_color, (xy[0], xy[1], size[0], size[1]))
-    screen.fill(bg_color, (xy[0]+1, xy[1]+1, size[0]-2, size[1]-2))
-    print_multiline(screen, string_to_print, box_font, size[0]-10,
-            (xy[0]+5, xy[1]+5), text_color)
-    menu_buttons = {}
-    menu_buttons[buttons.make_norm_button((xy[0]+size[0]/2-50,
-            xy[1]+size[1]+5), (100, 50), "OK", "O", font[1][30])] = always(True)
-
-    buttons.show_buttons(menu_buttons)
-
-#create dialog with YES/NO buttons.
-def create_yesno(string_to_print, box_font, xy, size, bg_color, out_color,
-            text_color, button_names=("YES", "NO"), reverse_key_context = False):
-    screen.fill(out_color, (xy[0], xy[1], size[0], size[1]))
-    screen.fill(bg_color, (xy[0]+1, xy[1]+1, size[0]-2, size[1]-2))
-    print_multiline(screen, string_to_print, box_font, size[0]-10,
-                (xy[0]+5, xy[1]+5), text_color)
-    menu_buttons = {}
-    if button_names == ("YES", "NO"):
-        menu_buttons[buttons.make_norm_button((xy[0]+size[0]/2-110,
-                xy[1]+size[1]+5), (100, 50), button_names[0], "Y", font[1][30])] = always(True)
-        menu_buttons[buttons.make_norm_button((xy[0]+size[0]/2+10,
-                xy[1]+size[1]+5), (100, 50), button_names[1], "N", font[1][30])] = always(False)
-    else:
-        menu_buttons[buttons.make_norm_button((xy[0],
-                xy[1]+size[1]+5), -1, button_names[0], button_names[0][0], font[1][30])] = always(True)
-        menu_buttons[buttons.make_norm_button((xy[0]+size[0]-100,
-                xy[1]+size[1]+5), -1, button_names[1], button_names[1][0], font[1][30])] = always(False)
-
-
-    default = False
-    if reverse_key_context:
-        default = True
-
-    return buttons.show_buttons(menu_buttons, 
-                               key_callback=buttons.simple_key_handler(default))
-
-valid_input_characters = ('a','b','c','d','e','f','g','h','i','j','k','l','m',
-                        'n','o','p','q','r','s','t','u','v','w','x','y','z',
-                        'A','B','C','D','E','F','G','H','I','J','K','L','M',
-                        'N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
-                        '0','1','2','3','4','5','6','7','8','9','.',' ')
-
-def create_textbox(descript_text, starting_text, box_font, xy, size,
-            max_length, bg_color, out_color, text_color, text_bg_color):
-    screen.fill(out_color, (xy[0], xy[1], size[0], size[1]))
-    screen.fill(bg_color, (xy[0]+1, xy[1]+1, size[0]-2, size[1]-2))
-    screen.fill(out_color, (xy[0]+5, xy[1]+size[1]-30, size[0]-10, 25))
-    print_multiline(screen, descript_text, box_font,
-                                size[0]-10, (xy[0]+5, xy[1]+5), text_color)
-
-    # Cursor starts at the end.
-    global cursor_loc, work_string
-    cursor_loc = len(starting_text)
-
-    def give_text():
-        return work_string
-
-    menu_buttons = {}
-    menu_buttons[buttons.make_norm_button((xy[0]+size[0]/2-50,
-            xy[1]+size[1]+5), (100, 50), "OK", "", font[1][30])] = give_text
-
-    work_string = starting_text
-    sel_button = -1
-
-    key_down_dict = {
-        pygame.K_BACKSPACE: False,
-        pygame.K_DELETE: False,
-        pygame.K_LEFT: False,
-        pygame.K_RIGHT: False
-    }
-    key_down_time_dict = {
-        pygame.K_BACKSPACE: 0,
-        pygame.K_DELETE: 0,
-        pygame.K_LEFT: 0,
-        pygame.K_RIGHT: 0
-    }
-    repeat_timing_dict = {
-        pygame.K_BACKSPACE: 6,
-        pygame.K_DELETE: 6,
-        pygame.K_LEFT: 6,
-        pygame.K_RIGHT: 6
-    }
-
-    def on_tick(tick_len):
-        global cursor_loc, work_string
-        need_redraw = False
-
-        keys = (pygame.K_BACKSPACE, pygame.K_DELETE, pygame.K_LEFT, 
-                pygame.K_RIGHT)
-        backspace, delete, left, right = keys
-        for key in keys:
-            if key_down_time_dict[key]:
-                i_need_redraw = False
-
-                key_down_time_dict[key] += 1
-                if key_down_time_dict[key] > repeat_timing_dict[key]:
-                    key_down_time_dict[key] = 1
-                    if repeat_timing_dict[key] > 1:
-                        repeat_timing_dict[key] -= 1
-
-                    i_need_redraw = True
-                    if key == backspace and cursor_loc > 0:
-                        work_string = work_string[:cursor_loc-1] + \
-                                      work_string[cursor_loc:]
-                        cursor_loc -= 1
-                    elif key == delete and cursor_loc < len(work_string):
-                        work_string = work_string[:cursor_loc] + \
-                                      work_string[cursor_loc+1:]
-                    elif key == left and cursor_loc > 0:
-                        cursor_loc -= 1
-                    elif key == right and cursor_loc < len(work_string):
-                        cursor_loc += 1
-                    else:
-                        # Nothing happened.
-                        i_need_redraw = False
-
-                if not key_down_dict[key]:
-                    key_down_time_dict[key] = 0
-                    repeat_timing_dict[key] = 6
-
-                need_redraw = need_redraw or i_need_redraw
-
-        return need_redraw
-
-    def do_refresh():
-        draw_cursor_pos = box_font.size(work_string[:cursor_loc])
-        screen.fill(text_bg_color, (xy[0]+6, xy[1]+size[1]-29,
-                    size[0]-12, 23))
-        screen.fill(text_color, (xy[0]+6+draw_cursor_pos[0], xy[1]+size[1]-28,
-                1, draw_cursor_pos[1]))
-        print_string(screen, work_string, box_font, -1, (xy[0]+7,
-                    xy[1]+size[1]-28), text_color)
-
-    def on_key_down(event):
-        key = event.key
-        global cursor_loc, work_string
-        if key in (pygame.K_RETURN, pygame.K_KP_ENTER):
-            return work_string
-        if key == pygame.K_ESCAPE:
-            return ""
-
-        if event.unicode in valid_input_characters:
-            if cursor_loc < max_length:
-                work_string = work_string[:cursor_loc]+event.unicode+ \
-                                            work_string[cursor_loc:]
-                cursor_loc += 1
-            return
-
-        # Mark the key as down.
-        key_down_dict[key] = True
-        # And force it to trigger immediately.
-        key_down_time_dict[key] = 6
-
-    def on_key_up(event):
-        key = event.key
-        # Mark the key as up, but don't clear its down time.
-        key_down_dict[key] = False
-
-    def on_click(event):
-        global cursor_loc
-        if event.button == 1:
-            if (event.pos[0] > xy[0]+6 and event.pos[1] > xy[1]+size[1]-29 and
-             event.pos[0] < xy[0]+size[0]-6 and event.pos[1] < xy[1]+size[1]-6):
-                cursor_x = event.pos[0] - (xy[0]+6)
-                prev_x = 0
-                for i in range(1, len(work_string)):
-                    curr_x = box_font.size(work_string[:i])[0]
-                    if (curr_x + prev_x) / 2 >= cursor_x:
-                        cursor_loc=i-1
-                        break
-                    elif curr_x >= cursor_x:
-                        cursor_loc=i
-                        break
-                    prev_x = curr_x
-                else:
-                    cursor_loc=len(work_string)
-
-    return buttons.show_buttons(menu_buttons, click_callback=on_click,
-                                  tick_callback=on_tick, 
-                                  key_callback=on_key_down,
-                                  keyup_callback=on_key_up,
-                                  refresh_callback=do_refresh)
-
-#creates a box, as used throughout the game.
-def create_norm_box(xy, size, outline_color="white", inner_color="dark_blue"):
-    screen.fill(colors[outline_color], (xy[0], xy[1], size[0], size[1]))
-    screen.fill(colors[inner_color], (xy[0]+1, xy[1]+1, size[0]-2, size[1]-2))
-
 
 #Takes a number and adds commas to it to aid in human viewing.
 def add_commas(number):
