db072adc52d8866c383e0971eb3221de5cd1762c
evilmrhenry <evilmrhenry@b907cd06-5c16-4c62-a1d0-e7f4b8dee1c7>
2009-01-16 05:36:38 +0000

    * ./trunk/code/screens/knowledge.py 	Finished work with knowledge screen.

--- code/screens/knowledge.py
+++ code/screens/knowledge.py
@@ -18,90 +18,9 @@
 
 #This file is used to display the knowledge lists.
 
+import pygame
 from code import g
 from code.graphics import text, button, dialog, widget, constants, listbox, g as gg
-
-
-#def display_items(item_type):
-    #list_size = 16
-    #list = []
-    #display_list = []
-
-    #if item_type == "tech":
-        #items = [tech for tech in g.techs.values() if tech.available()]
-    #elif item_type == "concept":
-        #items = [ [item[1][0], item[0]] for item in g.help_strings.items()]
-        #items.sort()
-    #else:
-        #items = [item for item in g.items.values() 
-                      #if item.item_type == item_type and item.available()]
-
-    #if item_type != "concept":
-        #items = [ [item.name, item.id ] for item in items]
-        #items.sort()
-
-    #for name, id in items:
-        #list.append(id)
-        #display_list.append(name)
-
-    #xy_loc = (g.screen_size[0]/2 - 289, 50)
-    #listbox.resize_list(list, list_size)
-
-    #menu_buttons = {}
-    #menu_buttons[buttons.make_norm_button((xy_loc[0]+103, xy_loc[1]+367), (100, 50), "BACK", "B", g.font[1][30])] = listbox.exit
-
-    #def do_refresh(item_pos):
-        #if item_type == "tech":
-            #refresh_tech(list[item_pos], xy_loc)
-        #elif item_type == "concept":
-            #refresh_concept(list[item_pos], xy_loc)
-        #else:
-            #refresh_items(list[item_pos], xy_loc)
-
-    #listbox.show_listbox(display_list, menu_buttons, 
-                         #list_size=list_size,
-                         #loc=xy_loc, box_size=(230, 350), 
-                         #pos_callback=do_refresh, return_callback=listbox.exit)
-    ##details screen
-
-
-#def refresh_items(item_name, xy):
-    #xy = (xy[0]+100, xy[1])
-    #g.screen.fill(g.colors["white"], (xy[0]+155, xy[1], 300, 350))
-    #g.screen.fill(g.colors["dark_blue"], (xy[0]+156, xy[1]+1, 298, 348))
-    #if item_name == "": 
-        #return
-    #g.print_string(g.screen, g.items[item_name].name,
-            #g.font[0][22], -1, (xy[0]+160, xy[1]+5), g.colors["white"])
-
-    ##Building cost
-    #string = "Building Cost:"
-    #g.print_string(g.screen, string,
-            #g.font[0][18], -1, (xy[0]+160, xy[1]+30), g.colors["white"])
-
-    #string = g.to_money(g.items[item_name].cost[0])+" Money"
-    #g.print_string(g.screen, string,
-            #g.font[0][16], -1, (xy[0]+160, xy[1]+50), g.colors["white"])
-
-    #string = g.to_time(g.items[item_name].cost[2])
-    #g.print_string(g.screen, string,
-            #g.font[0][16], -1, (xy[0]+160, xy[1]+70), g.colors["white"])
-
-    ##Quality
-    #if g.items[item_name].item_type == "compute":
-        #string = "CPU per day: "+str(g.items[item_name].item_qual)
-    #elif g.items[item_name].item_type == "react":
-        #string = "Detection chance reduction: "+g.to_percent(g.items[item_name].item_qual)
-    #elif g.items[item_name].item_type == "network":
-        #string = "CPU bonus: "+g.to_percent(g.items[item_name].item_qual)
-    #elif g.items[item_name].item_type == "security":
-        #string = "Detection chance reduction: "+g.to_percent(g.items[item_name].item_qual)
-    #g.print_string(g.screen, string,
-            #g.font[0][20], -1, (xy[0]+160, xy[1]+90), g.colors["white"])
-
-    #g.print_multiline(g.screen, g.items[item_name].description,
-            #g.font[0][18], 290, (xy[0]+160, xy[1]+120), g.colors["white"])
-
 
 
 class KnowledgeScreen(dialog.Dialog):
@@ -109,11 +28,11 @@ class KnowledgeScreen(dialog.Dialog):
         super(KnowledgeScreen, self).__init__(*args, **kwargs)
 
         self.knowledge_type_list = ("Techs", "Items", "Concepts")
-        #self.desc_func = self.on_change
-        self.cur_knowledge_type = "Techs"
+        self.cur_knowledge_type = ""
         self.cur_knowledge = None
         self.knowledge_inner_list = ()
         self.knowledge_inner_list_key = ()
+        self.cur_focus = 0
 
         self.knowledge_choice = \
             listbox.UpdateListbox(self, (0.05, .18), (.21, .25),
@@ -132,9 +51,41 @@ class KnowledgeScreen(dialog.Dialog):
         self.back_button = button.ExitDialogButton(self, (0.17, 0.46), (-.3, -.1),
                                                    anchor=constants.TOP_LEFT,
                                                    text="BACK", hotkey="b")
+
+        #Set up the key handling.
+        #This is likely not the best way to do it.
+
+        self.remove_key_handler(pygame.K_UP, self.knowledge_choice.got_key)
+        self.remove_key_handler(pygame.K_DOWN, self.knowledge_choice.got_key)
+        self.remove_key_handler(pygame.K_PAGEUP, self.knowledge_choice.got_key)
+        self.remove_key_handler(pygame.K_PAGEDOWN, self.knowledge_choice.got_key)
+
+        self.remove_key_handler(pygame.K_UP, self.knowledge_inner.got_key)
+        self.remove_key_handler(pygame.K_DOWN, self.knowledge_inner.got_key)
+        self.remove_key_handler(pygame.K_PAGEUP, self.knowledge_inner.got_key)
+        self.remove_key_handler(pygame.K_PAGEDOWN, self.knowledge_inner.got_key)
+
+        self.add_key_handler(pygame.K_UP, self.key_handle)
+        self.add_key_handler(pygame.K_DOWN, self.key_handle)
+        self.add_key_handler(pygame.K_LEFT, self.key_handle)
+        self.add_key_handler(pygame.K_RIGHT, self.key_handle)
+
         self.set_knowledge_type(0)
+        self.set_knowledge(0)
 
 
+    #custom key handler.
+    def key_handle(self, event):
+        if event.type != pygame.KEYDOWN: return
+        if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+            self.cur_focus = (self.cur_focus + 1) % 2
+        else:
+            if self.cur_focus == 0:
+                self.knowledge_choice.got_key(event)
+            elif self.cur_focus == 1:
+                self.knowledge_inner.got_key(event)
+
+    #fill the right-hand listbox
     def set_inner_list(self, item_type):
         if item_type == "Techs":
             items = [tech for tech in g.techs.values() if tech.available()]
@@ -156,7 +107,7 @@ class KnowledgeScreen(dialog.Dialog):
             return_list2.append(name)
         return return_list1, return_list2
 
-
+    #Make sure the left listbox is correct after moving around.
     def set_knowledge_type(self, list_pos):
         if getattr(self, "knowledge_choice", None) is None:
             self.knowledge_inner_list_key, self.knowledge_inner_list = \
@@ -168,7 +119,10 @@ class KnowledgeScreen(dialog.Dialog):
         if prev_know != self.cur_knowledge_type:
             self.knowledge_inner_list_key, self.knowledge_inner.list = \
                         self.set_inner_list(self.cur_knowledge_type)
+            self.knowledge_inner.list_pos = 0
+            self.set_knowledge(0)
 
+    #Make sure the right-hand listbox is correct.
     def set_knowledge(self, list_pos):
         if getattr(self, "knowledge_inner", None) is None:
             return # Not yet initialized.
@@ -179,10 +133,8 @@ class KnowledgeScreen(dialog.Dialog):
             self.show_info(self.cur_knowledge_type,
                     self.knowledge_inner_list_key[list_pos])
 
+    #print information to the right.
     def show_info(self, knowledge_type, knowledge_key):
-        #print knowledge_type
-        #print knowledge_key
-
         desc_text = ""
 
         if knowledge_type == "Concepts":
@@ -190,7 +142,7 @@ class KnowledgeScreen(dialog.Dialog):
                         g.help_strings[knowledge_key][1]
         if knowledge_type == "Techs":
             desc_text = g.techs[knowledge_key].name + "\n\n"
-            #Building cost
+            #Cost
             if not g.techs[knowledge_key].done:
                 desc_text += "Research Cost:\n" + \
                         g.to_money(g.techs[knowledge_key].cost_left[0])+" Money, "
@@ -241,8 +193,6 @@ class KnowledgeScreen(dialog.Dialog):
                     background_color=gg.colors["dark_blue"],
                     align=constants.LEFT, valign=constants.TOP,
                     borders=constants.ALL)
-
-
 
 
     def show(self):
