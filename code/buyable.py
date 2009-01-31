3300f38a60a8d513d14197de4546a0a0321be3df
funnyman3595 <funnyman3595@b907cd06-5c16-4c62-a1d0-e7f4b8dee1c7>
2009-01-31 22:40:13 +0000

    Fixed issue 72 (cash jump when building a second batch of processors in the same base).

--- code/buyable.py
+++ code/buyable.py
@@ -176,7 +176,8 @@ class Buyable(object):
 
         # And apply it.
         was_complete = self.cost_paid
-        cost_paid = numpy.cast[numpy.int64](numpy.ceil(raw_paid))
+        cost_paid = numpy.maximum(numpy.cast[numpy.int64](numpy.ceil(raw_paid)),
+                                  was_complete)
         spent = cost_paid - was_complete
         return spent, cost_paid
 
