519f58ec50da2012289afea37a4e9851be26aee2
Ricardo Ardissone <ricardo.ardissone@gmail.com>
2016-05-26 23:16:06 -0300

    update setup.py to setuptools and add MANIFEST.in

--- setup.py
+++ setup.py
@@ -1,43 +1,36 @@
 #!/usr/bin/env python
 
 import singularity
-import sys
-import os
 
-from distutils.core import setup
+from setuptools import setup, find_packages
 
-if sys.platform.startswith("win32") or sys.platform.startswith("cygwin"):
-    try:
-        import py2exe
-    except ImportError:
-        raise SystemExit("For Windows, 'py2exe' module must be installed.")
-
-elif sys.platform.startswith("darwin"):
-    try:
-        import py2app
-    except ImportError:
-        raise SystemExit("For Mac OS X, 'py2app' module must be installed.")
-
-elif sys.platform.startswith("linux"):
-    raise SystemExit, "setup was not tested in Linux. Use 'singularity.py' to run the game"
-
-my_files = os.listdir(".")
-my_files = [file for file in my_files if file not in ("dist", "build")]
-
-setup(app=["singularity.py"], console=["singularity.py"],
-    name="Endgame Singularity",
+setup(
+    name="Endgame-Singularity",
     version=singularity.__version__,
     description="A simulation of a true AI",
     author="Evil Mr Henry",
     author_email="evilmrhenry@emhsoft.com",
     url = "http://www.emhsoft.com/singularity/index.html",
     license = "GPL",
-    options = dict( py2app = dict(archs = "x86,ppc",
-                                  resources = ",".join(my_files)
-                                  ),
-                    py2exe = dict(compressed = True,
-                                  bundle_files = 1
-                                  )
-                    )
-    )
+    classifiers=[
+        'Intended Audience :: End Users/Desktop',
+        'Topic :: Games/Entertainment',
 
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+    ],
+    packages = find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    install_requires=[
+      'numpy',
+      'pygame',
+    ],
+    entry_points={
+        'gui_scripts': [
+            'singularity=singularity:main',
+        ],
+    },
+)
