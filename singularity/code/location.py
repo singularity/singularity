2925510506629cd280c4b4f99d451000e474fa45
Ricardo Ardissone <ricardo.ardissone@gmail.com>
2016-05-29 16:16:50 -0300

    libfuturize.fixes.fix_basestring

--- singularity/code/location.py
+++ singularity/code/location.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import
 from __future__ import unicode_literals
 from __future__ import division
 from __future__ import print_function
+from past.builtins import basestring
 from future import standard_library
 standard_library.install_aliases()
 from builtins import *
