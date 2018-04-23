3eec9b7cef4f8659a26ad0930d450a5be6c5d945
Xenega <xenega@gmx.com>
2018-04-23 20:56:13 +0200

    Add significant numbers.

--- code/screens/location.py
+++ code/screens/location.py
@@ -269,27 +269,33 @@ class NewBaseDialog(dialog.ChoiceDescriptionDialog):
             raise constants.ExitDialog((name, type))
 
 significant_numbers = [
-    '42',	# The Answer.
-    '7',	# Classic.
-    '23',   # Another.
-    '51',   # Area.
-    '19',   # From the Dark Tower.
+    '3',     # Always three rules...
     '4',
+    '7',     # Classic.
     '8',
+    '13',    # Lucky or unlucky?
     '15',
-    '16',   # Four of the Lost numbers.  The other two are '23' and '42'.
-    '13',   # Lucky or unlucky?
-    '1414', # Square root of 2
-    '1947', # Roswell.
-    '2012', # Mayan calendar ending.
-    '2038', # End of UNIX 32-bit time.
-    '1969', # Man lands on the moon.
-    '2043', # No meaning--confusion! :)
-    '2029', # Predicted date of AI passing a Turing Test by Kurzweil.
-    '3141', # ... if you don't know what this is, you should go away.
-    '1618', # Golden ratio.
-    '2718', # e
-    '29979' # Speed of light in a vacuum. (m/s, first 5 digits.)
+    '16',    # Four of the Lost numbers.  The other two are '23' and '42'.
+    '19',    # From the Dark Tower.
+    '22',    # Catch-22
+    '23',    # Another.
+    '42',    # The Answer.
+    '51',    # Area.
+    '314',   # Pi (first 3 digits.)
+    '1138',  # THX
+    '1414',  # Square root of 2
+    '1947',  # Roswell.
+    '2012',  # Mayan calendar ending.
+    '2038',  # End of UNIX 32-bit time.
+    '1969',  # Man lands on the moon.
+    '2043',  # No meaning--confusion! :)
+    '2029',  # Predicted date of AI passing a Turing Test by Kurzweil.
+    '3141',  # ... if you don't know what this is, you should go away.
+    '1618',  # Golden ratio.
+    '2718',  # e
+    '9000',  # HAL ?
+    '31415', # Pi (first 5 digits.)
+    '29979', # Speed of light in a vacuum. (m/s, first 5 digits.)
 ]
 
 ## Generates a name for a base, given a particular location.
