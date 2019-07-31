d7e4aeeebc173240d43cd5888e4cefa2498da6b5
Niels Thykier <niels@thykier.net>
2019-07-31 17:29:10 +0000

    Python3 compatible octal literal
    Signed-off-by: Niels Thykier <niels@thykier.net>

--- code/dirs.py
+++ code/dirs.py
@@ -255,7 +255,7 @@ def get_readable_i18n_files(filename, lang=None, default_language=True,
 
 def makedirs_if_not_exist(directory):
     try:
-        os.makedirs(directory, mode=0700)
+        os.makedirs(directory, mode=0o0700)
     except OSError as e:
         if e.errno != errno.EEXIST:
             raise
