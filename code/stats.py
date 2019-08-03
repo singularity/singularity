a7427e47ac6a118079210b3cea0c25dc0b2d3190
Xenega <xenega@gmx.com>
2019-08-03 17:06:04 +0200

    Add the new statistics code.

---
+++ code/stats.py
@@ -0,0 +1,57 @@
+#file: statistics.py
+#Copyright (C) 2008 FunnyMan3595
+#This file is part of Endgame: Singularity.
+
+#Endgame: Singularity is free software; you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation; either version 2 of the License, or
+#(at your option) any later version.
+
+#Endgame: Singularity is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+
+#You should have received a copy of the GNU General Public License
+#along with Endgame: Singularity; if not, write to the Free Software
+#Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+#This file contains the Statistic class, used for saving/loading single-game
+#statistics.
+
+import g
+
+class Statistics(object):
+    
+    enabled = True
+    
+    def __init__(self):
+        super(Statistics, self).__init__()
+        self._stats = {}
+    
+    def __len__(self):
+        len(self._stats)
+    
+    def __setitem__(self, key, value):
+        self._stats[key] = value
+    
+    def __getitem__(self, key):
+        stat = self._stats.get(key, None)
+        
+        if (stat is None):
+            stat = Statistic(key)
+            self._stats[key] = stat
+        
+        return stat
+    
+    def __iter__(self):
+        return self._stats.__iter__()
+
+
+class Statistic(object):
+    def __init__(self, name):
+        self._name = name
+        self.value = 0
+
+itself = Statistics()
+
