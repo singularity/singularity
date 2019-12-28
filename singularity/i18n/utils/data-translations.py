0b842cc5bac945ce85f7caec63ee1ad97c26d627
Niels Thykier <niels@thykier.net>
2019-12-28 21:20:14 +0000

    Simplify data-translations.py
    Signed-off-by: Niels Thykier <niels@thykier.net>

--- singularity/i18n/utils/data-translations.py
+++ singularity/i18n/utils/data-translations.py
@@ -6,10 +6,11 @@ import os.path
 from io import open
 import ConfigParser
 
+
 def get_esdir(myname):
-    mydir  = os.path.dirname(myname)
-    esdir  = os.path.abspath(os.path.join(os.path.dirname(myname), '../..'))
+    esdir = os.path.abspath(os.path.join(os.path.dirname(myname), '../..'))
     return esdir
+
 
 def build_option_parser():
     import argparse
@@ -17,27 +18,18 @@ def build_option_parser():
     description = '''Find data strings and save them for translation.'''
 
     parser = argparse.ArgumentParser(description=description)
-    parser.add_argument("-d", "--directory", dest="directory", default=None,
-    help="Use E:S root directory DIR (default dirname(__file__)/../..)", metavar="DIR")
-    parser.add_argument("-l", "--lang", dest="lang", default=None,
-    help="Add translation for language", metavar="LANG")
     parser.add_argument("-o", "--output", dest="output", default=None,
-    help="PO/POT File output", metavar="FILE")
+                        help="PO/POT File output", metavar="FILE")
 
     return parser.parse_args()
 
+
 def main():
     args = build_option_parser()
-    
-    global esdir
-    if (args.directory):
-        esdir = osp.abspath(args.directory)
-    else:
-        esdir = get_esdir(sys.argv[0])
-    
-    generate_translations(args.output, args.lang)
+    generate_translations(args.output)
 
-def generate_translations(output_file, lang):
+
+def generate_translations(output_file):
     esdir = get_esdir(__file__)
     sys.path.insert(0, esdir)
 
@@ -69,32 +61,18 @@ msgstr ""
 
     po = polib.pofile(output_file)
 
-    datadir = esdir + "/data"
+    datadir = os.path.join(esdir, "data")
     file_list = os.listdir(datadir)
 
     for filename in sorted(file_list):
         if not filename.endswith("_str.dat"):
             continue
         
-        filepath = datadir + "/" +  filename
-        
-        try:
-            fd = open(filepath, encoding='utf-8')
+        filepath = os.path.join(datadir, filename)
+
+        with open(filepath, encoding='utf-8') as fd:
             config = ConfigParser.RawConfigParser()
             config.readfp(fd)
-            
-            lang_fd = None
-            if lang is not None:
-                lang_file = esdir + "/i18n/lang_" + lang + "/" + filename
-        
-                try:
-                    lang_fd = open(lang_file, encoding='utf-8')
-                    lang_config = ConfigParser.RawConfigParser()
-                    lang_config.readfp(lang_fd)
-                except Exception:
-                    if lang_fd is not None:
-                        lang_fd.close()
-                    lang_fd = None
 
             for section_id in config.sections():
                 for option in config.options(section_id):
@@ -111,32 +89,11 @@ msgstr ""
                     entry.msgctxt = ctxt
                     entry.msgstr = ""
 
-                    if lang_fd is not None:
-                        try:
-                            tr_text = lang_config.get(section_id, option).strip()
-                            entry.msgstr = tr_text
-                        except (ConfigParser.NoOptionError, ConfigParser.NoSectionError):
-                            entry.msgstr = ""
-
-        finally:
-            if fd is not None:
-                fd.close()
-            if lang_fd is not None:
-                lang_fd.close()
-
-
     po.save(output_file)
+
 
 if __name__ == '__main__':
     try:
         sys.exit(main())
-    except Exception as e:
-        import traceback
-        ex_type, ex, tb = sys.exc_info()
-        print(ex)
-        traceback.print_tb(tb)
-        sys.exit(1)
     except KeyboardInterrupt:
         pass
-    except SystemExit as ex:
-        raise ex
