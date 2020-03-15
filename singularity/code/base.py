singularity/singularity

commit ce30e320537c6e75caadd0ab9d524a30a5288b75

Let Buyable.{name,description} point to BuyableSpec.{name,description}
... with the exception of Base.name as bases have unique
non-translatable names.

This makes it easier to update translations as it now just requires us
to update the translations for BuyableSpec and it is automatically
reflected in the Buyable instances.

Closes: #232
Signed-off-by: Niels Thykier <niels@thykier.net>

singularity/code/base.py
@@ -146,7 +146,7 @@ class Base(buyable.Buyable):
     def __init__(self, name, spec, built=False):
         super(Base, self).__init__(spec)
 
-        self.name = name
+        self._name = name
         self.started_at = g.pl.raw_min
 
         self.location = None

singularity/code/buyable.py
@@ -111,8 +111,6 @@ class BuyableSpec(spec.GenericSpec, prerequisite.Prerequisite):
 class Buyable(object):
     def __init__(self, spec, count=1):
         self.spec = spec
-        self.name = spec.name
-        self.description = spec.description
         self.prerequisites = spec.prerequisites
 
         self.total_cost = spec.cost * count
@@ -125,6 +123,16 @@ class Buyable(object):
     @property
     def id(self):
         return self.spec.id
+
+    @property
+    def name(self):
+        if hasattr(self, '_name'):
+           return self._name
+        return self.spec.name
+
+    @property
+    def description(self):
+        return self.description
 
     # Note that this is a method, handled by a property to avoid confusing
     # pickle.

singularity/code/savegame.py
@@ -654,6 +654,8 @@ def _convert_base(base, save_version):
 
     if ("power_state" in base.__dict__):
         base._power_state = base.__dict__["power_state"]
+
+    base._name = base.__dict__['name']
         
     return base
 

commit 844c25a37160f68b7b6b54b9b1b0ea9fe9ad3247

Fix regression (crash) when renaming a base
Fixes: ce30e320537c6e75caadd0ab9d524a30a5288b75
Signed-off-by: Niels Thykier <niels@thykier.net>

singularity/code/base.py
@@ -174,6 +174,10 @@ class Base(buyable.Buyable):
 
         self.maintenance = buyable.array(self.spec.maintenance, int64)
 
+    @buyable.Buyable.name.setter
+    def name(self, value):
+        self._name = value
+
     @property
     def cpus(self):
         return self.items.get("cpu", None)