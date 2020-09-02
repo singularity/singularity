singularity/singularity

commit 71efb3482dfe72ac210cef7365c76eeb4b6841f2

Event cheat: Support triggering and expiring events
Closes: #290
Signed-off-by: Niels Thykier <niels@thykier.net>

singularity/code/event.py
@@ -98,12 +98,15 @@ class Event(object):
         if not self.decayable_event:
             return
 
-        if self.is_expired:
-            self.effect.undo_effect()
-            self.triggered = 0
-            self.triggered_at = -1
+        if self.is_past_expiry_date:
+            self.expire_now()
 
-    def is_expired(self):
+    def expire_now(self):
+        self.effect.undo_effect()
+        self.triggered = 0
+        self.triggered_at = -1
+
+    def is_past_expiry_date(self):
         if not self.decayable_event:
             return False
         if g.pl.raw_sec - self.triggered_at > self.duration * g.seconds_per_day:
@@ -133,7 +136,7 @@ class Event(object):
             # be triggered "now".
             obj.triggered_at = obj_data.get('triggered_at', g.pl.raw_sec)
 
-            if obj.is_expired():
+            if obj.is_past_expiry_date():
                 # Can happen if the duration is reduced after the savegame was made
                 obj.triggered = 0
                 obj.triggered_at = -1
@@ -142,9 +145,6 @@ class Event(object):
         return obj
 
     def trigger(self, loading_savegame=False):
-        if not loading_savegame:
-            g.map_screen.show_message(self.description)
-
         self.triggered = 1
         if loading_savegame:
             # During a load, deserialize_obj will restore the original triggered_at time

singularity/code/player.py
@@ -441,20 +441,29 @@ class Player(object):
             event_target = self.events.get(event_id, None)
             
             # Skip events already flagged as triggered.
-            if event_target and event_target.triggered == 1:
+            if event_target and event_target.triggered:
                 continue
 
             if chance.roll_interval(event_spec.chance/10000., time_sec):
-                self.pause_game()
-
-                if not event_target:
-                    event_target = event.Event(event_spec)
-                    self.events[event_id] = event_target
-
-                event_target.trigger()
-                self.log.append(LogEmittedEvent(self.raw_sec, event_id))
+                self.trigger_event(event_spec)
                 return True  # Don't trigger more than one at a time.
         return False
+
+    def trigger_event(self, event_spec, show_event_description=True):
+        event_id = event_spec.id
+        event_target = self.events.get(event_id, None)
+
+        if not event_target:
+            event_target = event.Event(event_spec)
+            self.events[event_id] = event_target
+        elif event_target.triggered:
+            return
+
+        event_target.trigger()
+        if show_event_description:
+            self.pause_game()
+            g.map_screen.show_message(event_target.description)
+        self.log.append(LogEmittedEvent(self.raw_sec, event_id))
 
     def recalc_cpu(self):
         if (not self.initialized): return

singularity/code/screens/cheat_menu.py
@@ -34,9 +34,40 @@ class EventManipulationDialog(dialog.ChoiceDescriptionDialog):
         super(EventManipulationDialog, self).__init__(parent, pos, size, anchor,
                                                       *args, **kwargs)
 
+        self.selected_event_spec = None
+
         self.yes_button.parent = None
+
+        self.no_button.pos = (-.97,-.99)
+        self.no_button.size = (-.22, -.1)
+
+        self.trigger_event_button = button.FunctionButton(self, (-.27, -.99), (-.22, -.1),
+                                                          autotranslate=True,
+                                                          enabled=False,
+                                                          text=N_("Trigger"),
+                                                          anchor=constants.BOTTOM_LEFT,
+                                                          function=self._trigger_event)
+        self.expire_event_button = button.FunctionButton(self, (-.51, -.99), (-.22, -.1),
+                                                         autotranslate=True,
+                                                         enabled=False,
+                                                         text=N_("Expire"),
+                                                         anchor=constants.BOTTOM_LEFT,
+                                                         function=self._expire_event)
         self.description = ""
         self.desc_func = self.on_change
+
+    def _trigger_event(self):
+        g.pl.trigger_event(self.selected_event_spec, show_event_description=False)
+        self.needs_rebuild = True
+
+    def _expire_event(self):
+        event_instance = g.pl.events.get(self.selected_event_spec.id)
+        if not event_instance or not event_instance.triggered:
+            return
+        if not event_instance.decayable_event:
+            return
+        event_instance.expire_now()
+        self.needs_rebuild = True
 
     def show(self):
         self.key_list = sorted(g.events.values(),
@@ -47,6 +78,9 @@ class EventManipulationDialog(dialog.ChoiceDescriptionDialog):
         return super(EventManipulationDialog, self).show()
 
     def on_change(self, description_pane, event_spec):
+        self.selected_event_spec = event_spec
+        self.trigger_event_button.enabled = True
+        self.expire_event_button.enabled = False
         event_instance = g.pl.events.get(event_spec.id)
         triggered_state = _('NO')
         trigger_duration = g.to_time(event_spec.duration * g.minutes_per_day) \
@@ -55,6 +89,8 @@ class EventManipulationDialog(dialog.ChoiceDescriptionDialog):
         uniqueness = _('YES') if event_spec.unique else _('NO')
         if event_instance:
             if event_instance.triggered:
+                self.trigger_event_button.enabled = False
+                self.expire_event_button.enabled = event_instance.decayable_event
                 triggered_state = _('YES')
                 triggered_at_raw = event_instance.triggered_at
         text_format = _("{DESCRIPTION}\n\n" +

commit aecb3c561ee3e6082466b9c70d146ca9c96252f4

event.py: Fix usage of is_past_expiry_date
Fixes: 71efb3482dfe72ac210cef7365c76eeb4b6841f2
Signed-off-by: Niels Thykier <niels@thykier.net>

singularity/code/event.py
@@ -106,6 +106,7 @@ class Event(object):
         self.triggered = 0
         self.triggered_at = -1
 
+    @property
     def is_past_expiry_date(self):
         if not self.decayable_event:
             return False
@@ -136,7 +137,7 @@ class Event(object):
             # be triggered "now".
             obj.triggered_at = obj_data.get('triggered_at', g.pl.raw_sec)
 
-            if obj.is_past_expiry_date():
+            if obj.is_past_expiry_date:
                 # Can happen if the duration is reduced after the savegame was made
                 obj.triggered = 0
                 obj.triggered_at = -1