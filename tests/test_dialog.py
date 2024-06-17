f1da847bad75a12c965f29a9f3cd8d67d68c4fe2
wikazareba <wikazareba420@gmail.com>
2024-06-17 15:40:19 +0200

    function1 testing remove branch3

--- tests/test_dialog.py
+++ tests/test_dialog.py
@@ -1,6 +1,6 @@
 import pygame
 import unittest
-from singularity.code.graphics.dialog import fake_click, branch_coverage
+from singularity.code.graphics.dialog import fake_click, branch_coverage, Dialog
 from singularity.code import data
 from singularity.code.dirs import create_directories
 
@@ -9,19 +9,20 @@ def reset_branch_coverage():
     branch_coverage = {
         "branch_1": False,  
         "branch_2": False,  
-        "branch_3": False,
+       #"branch_3": False,
         "branch_4": False,
         "branch_5": False,  
     }
 
-    #def setup_module():
-    #create_directories(True)
-    #data.load_internal_id()
-
-
-class TestFakeClick(unittest.TestCase):
+class TestDialog(unittest.TestCase):
 
     def setUp(self):
+       # self.dialog_with_parent = Dialog()
+        #self.dialog_with_parent.parent = "parent"
+        
+        #self.dialog_without_parent = Dialog()
+        #self.dialog_without_parent.parent = None
+
         pygame.init()
         reset_branch_coverage()
 
@@ -29,14 +30,16 @@ class TestFakeClick(unittest.TestCase):
         pygame.quit()
 
     def test_fake_click_down(self):
-        fake_click(True)
+        #branch_coverage["branch_1"] = False
+        #branch_coverage["branch_2"] = False
+        fake_click(down = True)
         self.assertTrue(branch_coverage["branch_1"], "Branch 1 was not hit")
-        self.assertTrue(branch_coverage["branch_3"], "Branch 3 was not hit")
+        #self.assertTrue(branch_coverage["branch_3"], "Branch 3 was not hit")
 
     def test_fake_click_up(self):
-        fake_click(False)
+        fake_click(down = False)
         self.assertTrue(branch_coverage["branch_2"], "Branch 2 was not hit")
-        self.assertTrue(branch_coverage["branch_3"], "Branch 3 was not hit")
+        #self.assertTrue(branch_coverage["branch_3"], "Branch 3 was not hit")
 
 if __name__ == '__main__':
     unittest.main()
