a0516aee369064afd5d2a11d8072afd92a9284d1
wikazareba <wikazareba420@gmail.com>
2024-06-19 21:32:49 +0200

    funtion 2 test improvement

--- tests/test_dialog.py
+++ tests/test_dialog.py
@@ -1,8 +1,7 @@
 import pygame
 import unittest
 from singularity.code.graphics.dialog import fake_click, branch_coverage, insort_right_w_key
-#from singularity.code import data
-#from singularity.code.dirs import create_directories
+
 
 def reset_branch_coverage():
     global branch_coverage
@@ -28,42 +27,58 @@ class TestDialog(unittest.TestCase):
     def test_fake_click_down(self):
         fake_click(down = True)
         self.assertTrue(branch_coverage["branch_1"], "Branch 1 was not hit")
-        #self.assertTrue(branch_coverage["branch_3"], "Branch 3 was not hit")
+        
 
     def test_fake_click_up(self):
         fake_click(down = False)
         self.assertTrue(branch_coverage["branch_2"], "Branch 2 was not hit")
-        #self.assertTrue(branch_coverage["branch_3"], "Branch 3 was not hit")
+        
 
     def test_insert_into_empty_list(self):
         a = []
         insort_right_w_key(a, 5)
         self.assertEqual(a, [5])
+        self.assertTrue(branch_coverage["branch_5"], "Branch 5 was not hit")
 
     def test_insert_in_sorted_list(self):
         a = [1, 3, 5, 7]
         insort_right_w_key(a, 4)
         self.assertEqual(a, [1, 3, 4, 5, 7])
+        self.assertTrue(branch_coverage["branch_5"], "Branch 5 was not hit")
+        self.assertTrue(branch_coverage["branch_6"], "Branch 6 was not hit")
+        self.assertTrue(branch_coverage["branch_7"], "Branch 7 was not hit")
+
 
     def test_insert_to_rightmost_existing_element(self):
         a = [1, 2, 2, 3]
         insort_right_w_key(a, 2)
         self.assertEqual(a, [1, 2, 2, 2, 3])
+        self.assertTrue(branch_coverage["branch_5"], "Branch 5 was not hit")
+        self.assertTrue(branch_coverage["branch_7"], "Branch 7 was not hit")
 
     def test_insert_with_negative_lo(self):
         a = [1, 3, 5]
         with self.assertRaises(ValueError):
             insort_right_w_key(a, 4, lo=-1)
+        self.assertTrue(branch_coverage["branch_3"], "Branch 3 was not hit")
 
     def test_insert_with_none_hi(self):
         a = [1, 3, 5]
         insort_right_w_key(a, 4, hi=None)
         self.assertEqual(a, [1, 3, 4, 5])
+        self.assertTrue(branch_coverage["branch_4"], "Branch 4 was not hit")
+        self.assertTrue(branch_coverage["branch_5"], "Branch 5 was not hit")
+        self.assertTrue(branch_coverage["branch_6"], "Branch 6 was not hit")
+        self.assertTrue(branch_coverage["branch_7"], "Branch 7 was not hit")
+
 
     def test_insert_with_custom_key(self):
         a = [(1, 'one'), (3, 'three'), (5, 'five')]
         insort_right_w_key(a, (4, 'four'), key=lambda v: v[0])
         self.assertEqual(a, [(1, 'one'), (3, 'three'), (4, 'four'), (5, 'five')])
+        self.assertTrue(branch_coverage["branch_5"], "Branch 5 was not hit")
+        self.assertTrue(branch_coverage["branch_6"], "Branch 6 was not hit")
+        self.assertTrue(branch_coverage["branch_7"], "Branch 7 was not hit")
 
         
 
