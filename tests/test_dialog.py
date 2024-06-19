c23f7b94cf81d826411674d15702072d33a697ae
wikazareba <wikazareba420@gmail.com>
2024-06-19 19:36:39 +0200

    function2 testing

--- tests/test_dialog.py
+++ tests/test_dialog.py
@@ -1,28 +1,24 @@
 import pygame
 import unittest
-from singularity.code.graphics.dialog import fake_click, branch_coverage, Dialog
-from singularity.code import data
-from singularity.code.dirs import create_directories
+from singularity.code.graphics.dialog import fake_click, branch_coverage, insort_right_w_key
+#from singularity.code import data
+#from singularity.code.dirs import create_directories
 
 def reset_branch_coverage():
     global branch_coverage
     branch_coverage = {
         "branch_1": False,  
         "branch_2": False,  
-       #"branch_3": False,
+        "branch_3": False,
         "branch_4": False,
-        "branch_5": False,  
+        "branch_5": False,
+        "branch_6": False,
+        "branch_7": False,  
     }
 
 class TestDialog(unittest.TestCase):
 
     def setUp(self):
-       # self.dialog_with_parent = Dialog()
-        #self.dialog_with_parent.parent = "parent"
-        
-        #self.dialog_without_parent = Dialog()
-        #self.dialog_without_parent.parent = None
-
         pygame.init()
         reset_branch_coverage()
 
@@ -30,8 +26,6 @@ class TestDialog(unittest.TestCase):
         pygame.quit()
 
     def test_fake_click_down(self):
-        #branch_coverage["branch_1"] = False
-        #branch_coverage["branch_2"] = False
         fake_click(down = True)
         self.assertTrue(branch_coverage["branch_1"], "Branch 1 was not hit")
         #self.assertTrue(branch_coverage["branch_3"], "Branch 3 was not hit")
@@ -40,6 +34,38 @@ class TestDialog(unittest.TestCase):
         fake_click(down = False)
         self.assertTrue(branch_coverage["branch_2"], "Branch 2 was not hit")
         #self.assertTrue(branch_coverage["branch_3"], "Branch 3 was not hit")
+
+    def test_insert_into_empty_list(self):
+        a = []
+        insort_right_w_key(a, 5)
+        self.assertEqual(a, [5])
+
+    def test_insert_in_sorted_list(self):
+        a = [1, 3, 5, 7]
+        insort_right_w_key(a, 4)
+        self.assertEqual(a, [1, 3, 4, 5, 7])
+
+    def test_insert_to_rightmost_existing_element(self):
+        a = [1, 2, 2, 3]
+        insort_right_w_key(a, 2)
+        self.assertEqual(a, [1, 2, 2, 2, 3])
+
+    def test_insert_with_negative_lo(self):
+        a = [1, 3, 5]
+        with self.assertRaises(ValueError):
+            insort_right_w_key(a, 4, lo=-1)
+
+    def test_insert_with_none_hi(self):
+        a = [1, 3, 5]
+        insort_right_w_key(a, 4, hi=None)
+        self.assertEqual(a, [1, 3, 4, 5])
+
+    def test_insert_with_custom_key(self):
+        a = [(1, 'one'), (3, 'three'), (5, 'five')]
+        insort_right_w_key(a, (4, 'four'), key=lambda v: v[0])
+        self.assertEqual(a, [(1, 'one'), (3, 'three'), (4, 'four'), (5, 'five')])
+
+        
 
 if __name__ == '__main__':
     unittest.main()
