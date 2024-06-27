5321ee6ae2ab8027c959b7c7a8947fb1eb90e2ea
wikazareba <wikazareba420@gmail.com>
2024-06-27 13:44:13 +0200

    function fake_click test done

--- tests/test_dialog.py
+++ tests/test_dialog.py
@@ -1,16 +1,11 @@
 import pygame
 import unittest
-from singularity.code.graphics.dialog import fake_click, branch_coverage, insort_right_w_key
+from singularity.code.graphics.dialog import fake_click, branch_coverage
 
 
 def reset_branch_coverage():
     global branch_coverage
     branch_coverage.update({
-        "branch_1": False,
-        "branch_2": False,
-        "branch_3": False,
-        "branch_4": False,
-        "branch_5": False,
         "branch_6": False,
         "branch_7": False,
     })
@@ -38,36 +33,8 @@ class TestDialog(unittest.TestCase):
         fake_click(down = False)
         self.assertTrue(branch_coverage["branch_7"], "Branch 7 was not hit")
 
-    def test_insert_with_negative_lo(self):
-        a = [1, 3, 5]
-        with self.assertRaises(ValueError):
-            insort_right_w_key(a, 4, lo=-1)
-        self.assertTrue(branch_coverage["branch_1"], "Branch 1 was not hit")
 
-    def test_insert_with_none_hi(self):
-        a = [1, 3, 5]
-        insort_right_w_key(a, 4, hi=None)
-        self.assertEqual(a, [1, 3, 4, 5])
-        self.assertTrue(branch_coverage["branch_2"], "Branch 2 was not hit")
-  
-    def test_insert_into_empty_list(self):
-        a = []
-        insort_right_w_key(a, 5)
-        self.assertEqual(a, [5])
-        self.assertTrue(branch_coverage["branch_3"], "Branch 3 was not hit")
-
-    def test_insert_in_sorted_list(self):
-        a = [1, 3, 5, 7]
-        insort_right_w_key(a, 4)
-        self.assertEqual(a, [1, 3, 4, 5, 7])
-        self.assertTrue(branch_coverage["branch_4"], "Branch 4 was not hit")
-        
-    def test_insert_to_rightmost_existing_element(self):
-        a = [1, 2, 2, 3]
-        insort_right_w_key(a, 2)
-        self.assertEqual(a, [1, 2, 2, 2, 3])
-        self.assertTrue(branch_coverage["branch_5"], "Branch 5 was not hit")
-
-
+print_branch_coverage()
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main(exit=False)
+    
